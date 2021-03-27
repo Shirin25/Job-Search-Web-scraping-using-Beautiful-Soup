24bcf1d837dd7e9e1c17c4db43292325cdf6e577
Shirin Julka <56020942+Shirin25@users.noreply.github.com>
2021-03-27 12:14:31 +0530

    updated

--- test1.py
+++ test1.py
@@ -1,7 +1,7 @@
 import scraper
 from scraper import find_job_from
 
-attributes=['titles','companies','links','date_listed']
+attributes=['titles','companies','links','date_list']
 find_job_from('Indeed','software Engineer','london',attributes)
 
 
